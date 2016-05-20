7c3675962e99d6e3363a7e4afdf78bc09871bcee
Erickon1 <rober_erick@hotmail.com>
2016-05-20 15:04:00 -0500

    add views and template

--- dejango/save_data/views.py
+++ dejango/save_data/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_objet_or_404, redirect
+from .models import *
+
+
 
 # Create your views here.
+class GetUser(View):
+	def get(self,request):
+		template = "templates/main.html"
+		return render(request,template)
+
+	
+	def post(self,request):
+		name= request.POST.get('name','')
+		email= request.POST.get('email','')
+		user_data = User(
+			name=name,
+			email=email,
+			)
+		user_data.save()
+		return redirect('http://127.0.0.1:8000')
+
+
